34cdafd0c9d4bad4378b19acb707455843ea5d42
Intellipaat <110481066+rushtoaksith@users.noreply.github.com>
2024-02-28 19:05:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
-apiVersion: v1
-kind: Service
-metadata:
-  name: my-service
-spec:
-  type: NodePort
-  selector:
-    app: custom
-  ports:
-   - protocol: TCP
-     targetPort: 8080
-     port: 8080
-     nodePort: 30007
+provider "aws" {
+  secret_key = ""
+  access_key = ""
+  region = "us-west-1"
+}
+resource "aws_instance" "k8-M" {
+  ami = "ami-0da7657fe73215c0c"
+  instance_type = "t2.medium"
+  key_name = "Aksith-Don't delete"
+  tags = {
+    Name = "KMASTER"
+  }
+}
+
+resource "aws_instance" "k8-S1" {
+  ami = "ami-0da7657fe73215c0c"
+  instance_type = "t2.medium"
+  key_name = "Aksith-Don't delete"
+  tags = {
+    Name = "KSlave"
+  }
+}
